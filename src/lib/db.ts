adb80e4a86cead1c753294304b54f9e4215bf530
William Wilkinson <will.wilkinson@carebridgehealth.com>
2024-03-11 15:38:39 -0500

    Added drizzle orm, tailwind, and shadcn.

---
+++ src/lib/db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const connectionString = import.meta.env.VITE_ENV == 'local' ? 'postgres://postgres:postgres@localhost:5432/draft_day' : ''
+
+const client = postgres(connectionString)
+
+export const db = drizzle(client)
